498330d0ed1c8b4a792ef95626aa7fbd28f7fabb
rogutmichal <rogutmichal00@gmail.com>
2026-06-22 13:13:26 +0200

    add filters and charts

--- app.py
+++ app.py
@@ -16,13 +16,45 @@ def load_and_prepare_data():
 
 df = load_and_prepare_data()
 
+st.sidebar.header("Filters")
+cities = sorted(df["City"].unique())
+
+
+selected_cities = st.sidebar.multiselect(
+    "Select Cities",
+    cities,
+    default=cities
+)
+
+products = sorted(df["Product"].unique())
+
+selected_products = st.sidebar.multiselect(
+    "Select Products",
+    products,
+    default=products
+)
+
+
+managers = sorted(df["Manager"].unique())
+
+selected_managers = st.sidebar.multiselect(
+    "Select Managers",
+    managers,
+    default=managers
+)
+
+
+filtered_df = df[
+    df["City"].isin(selected_cities) & df["Product"].isin(selected_products) & df["Manager"].isin(selected_managers)
+]
+
 st.subheader(" Key Performance Indicators")
 
 
-total_revenue = df["Revenue"].sum()
-orders = len(df)
-avg_order_value = df["Revenue"].mean()
-unique_products = df["Product"].nunique()
+total_revenue = filtered_df["Revenue"].sum()
+orders = len(filtered_df)
+avg_order_value = filtered_df["Revenue"].mean()
+unique_products = filtered_df["Product"].nunique()
 
 col1, col2, col3, col4 = st.columns(4)
 
@@ -37,7 +69,7 @@ col4.metric("Products", unique_products)
 
 st.subheader(" Revenue by City")
 
-city_sales = group_revenue(df, "City")
+city_sales = group_revenue(filtered_df, "City")
 
 fig = px.bar(
     city_sales,
@@ -65,9 +97,9 @@ st.plotly_chart(fig, use_container_width=True)
 
 st.subheader("Revenue by Product")
 
-product_sales = group_revenue(df, "Product")
+product_sales = group_revenue(filtered_df, "Product")
 product_sales = (
-    group_revenue(df, "Product")
+    group_revenue(filtered_df, "Product")
     .head(5)
     .iloc[::-1]
 )
@@ -96,7 +128,7 @@ fig.update_yaxes(categoryorder="total ascending")
 st.subheader("Revenue Trend Over Time")
 
 daily_revenue = (
-    df.groupby("Date")["Revenue"]
+    filtered_df.groupby("Date")["Revenue"]
     .sum()
     .reset_index()
 )
@@ -124,6 +156,96 @@ best_day = daily_revenue.loc[
 ]
 
 st.info(
-    f"📅 Best sales day: {best_day['Date'].date()} "
+    f"Best sales day: {best_day['Date'].date()} "
     f"with revenue {best_day['Revenue']:,.0f}"
 )
+
+st.subheader("Manager Performance")
+
+manager_sales = group_revenue(filtered_df, "Manager")
+
+top_manager = manager_sales.iloc[0]
+
+st.success(
+    f"🏆 Top Manager: {top_manager['Manager']} | "
+    f"Revenue: {top_manager['Revenue']:,.0f}"
+)
+
+fig = px.bar(
+    manager_sales,
+    x="Manager",
+    y="Revenue",
+    text="Revenue",
+    title="Revenue by Manager"
+)
+
+fig.update_traces(
+    texttemplate='%{text:,.0f}',
+    textposition='outside'
+)
+
+fig.update_layout(
+    title_x=0.5,
+    xaxis_title="Manager",
+    yaxis_title="Revenue",
+    height=500
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    st.subheader("Revenue by Payment Method")
+
+    payment_sales = group_revenue(
+        filtered_df,
+        "Payment Method"
+    )
+
+
+    fig = px.pie(
+        payment_sales,
+        names="Payment Method",
+        values="Revenue",
+        title="Payments Method",
+        hole=0.4,
+    )
+
+    fig.update_layout(
+        title_x=0.5
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+    with col2:
+        st.subheader("Revenue by Purchase Type")
+
+        purchase_sales = group_revenue(
+            filtered_df,
+            "Purchase Type"
+        )
+
+        fig = px.pie(
+            purchase_sales,
+            names="Purchase Type",
+            values="Revenue",
+            hole=0.4,
+            title="Purchase Types"
+        )
+
+        fig.update_layout(
+            title_x=0.5
+        )
+
+        st.plotly_chart(
+            fig,
+            use_container_width=True
+        )
